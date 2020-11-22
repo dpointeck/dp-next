44e684d4166810df03d63b0c5a9da081f2d313ea
Daniel Pointecker <daniel.pointecker@gmail.com>
2020-11-22 19:38:35 +0100

    added site notice and privacy policy

---
+++ pages/site-notice.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+
+import PageLayout from '../layouts/pageLayout';
+import PageHeader from '@components/pageHeader';
+
+export default function SiteNoticePage() {
+  return (
+    <>
+      <Head>
+        <title>Site Notice</title>
+      </Head>
+      <PageLayout>
+        <div className='pt-0 pb-20'>
+          <PageHeader>Site Notice</PageHeader>
+          <article className='text-2xl max-w-2xl mx-auto relative z-50 mt-8'>
+            <h2 className='font-thin text-4xl mb-3'>Daniel Pointecker</h2>
+            <p className='mb-2'>
+              <b>Site description:</b> Portfolio & Blog for me - Daniel
+              Pointecker
+            </p>
+            <p className='mb-2'>
+              <b>Address:</b> 4776 Diersbach, Großwaging 10 | Austria
+            </p>
+            <p>
+              <b>E-Mail:</b> daniel.pointecker@gmail.com
+            </p>
+          </article>
+        </div>
+      </PageLayout>
+    </>
+  );
+}
