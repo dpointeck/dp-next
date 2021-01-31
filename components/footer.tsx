ebe24a9a47fe0c802d28ac22ec1a8fcddaf00688
Daniel Pointecker <daniel.pointecker@gmail.com>
2021-01-31 18:56:44 +0100

    added z-index to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="pt-6 pb-6 container mx-auto flex justify-center font-mono">
+    <footer className="pt-6 pb-6 relative z-50 container mx-auto flex justify-center font-mono">
       <nav className="text-xs">
         <Link href="/site-notice">
           <a className="mr-6">Site Notice</a>
