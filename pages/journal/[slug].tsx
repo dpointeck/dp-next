bee0e631160cabe9554dc1802fe96484e7eaf9c2
Daniel Pointecker <daniel.pointecker@gmail.com>
2021-11-28 10:52:04 +0100

    🪲

--- pages/journal/[slug].tsx
+++ pages/journal/[slug].tsx
@@ -2,7 +2,7 @@ import { GetStaticPaths } from "next";
 import { useRouter } from "next/router";
 import PageLayout from "@layouts/pageLayout";
 import Head from "next/head";
-import { getPostBySlug, getAllPosts } from "../../lib/postgh repo clone dpointeck/dp-next";
+import { getPostBySlug, getAllPosts } from "../../lib/post";
 
 export default function Post(props) {
   const router = useRouter();
