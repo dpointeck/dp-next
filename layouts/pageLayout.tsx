31f5839aa850c0d06e6b8a718f7ba3a1d661c242
Daniel Pointecker <daniel.pointecker@gmail.com>
2021-12-19 15:26:19 +0100

    another fixed type

--- layouts/pageLayout.tsx
+++ layouts/pageLayout.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledPageLayout = styled.div``;
 
-export default function PageLayout(props) {
+export default function PageLayout(props:any) {
   return (
     <StyledPageLayout className="layout-page container mx-auto p-4 md:px-10">
       {props.children}
