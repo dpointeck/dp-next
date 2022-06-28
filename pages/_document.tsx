13ddb4060e011cd93ecf0f3e0773f5bee53c2e2a
dpointeck <6PJWanbeAPsmjzWpwZRA>
2022-06-28 05:57:18 +0200

    🧅

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,9 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
-import { ServerStyleSheet } from "styled-components";
 import Footer from "@components/footer";
 import { ReactElement } from "react";
 
 class DpDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext):Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal();
-    }
-  }
-
+  
   render(): ReactElement {
     return (
       <Html lang="en">
