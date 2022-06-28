b9c64819aed544125fe3336e3d3edac408138309
dpointeck <6PJWanbeAPsmjzWpwZRA>
2022-06-28 05:53:35 +0200

    💅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
-import { ThemeProvider } from "styled-components";
 import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 
-import theme from "@styles/theme";
 
 import "../fonts/Inter/inter.css";
 import "../fonts/Greycliff/greycliff.css";
@@ -16,12 +14,10 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const isHome = router.pathname === "/";
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Nav />
-        <Component {...pageProps} />
-        <MobileNav />
-      </ThemeProvider>
+    <> 
+      <Nav />
+      <Component {...pageProps} />
+      <MobileNav />
     </>
   );
 }
--- pages/journal/index.tsx
+++ pages/journal/index.tsx
@@ -1,10 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
-import styled from "styled-components";
 import { getConfig, getPostsByYear } from "../../lib/post";
 import PageLayout from "@layouts/pageLayout";
 import PageHeader from "@components/pageHeader";
 import Plus from "@svg/Plus.js";
+import styles from "./index.module.scss";
 
 export default function JournalPage({ years }:any): unknown {
   return (
@@ -42,63 +42,9 @@ export async function getStaticProps() {
   };
 }
 
-const StyledPageHeading = styled.h1`
-  color: #1a202c;
-  position: relative;
-  z-index: 40;
-  font-size: 4rem;
-
-  &:before {
-    content: "";
-
-    display: block;
-    height: 80%;
-    width: 110%;
-    position: absolute;
-    top: 2.8rem;
-    left: 1.5;
-    z-index: -1;
-    background-image: linear-gradient(to top right, #fff95a, #fff22e);
-    border-radius: 1rem;
-  }
-`;
-
-const StyledPostsList = styled.div`
-  h3:not(first-child) {
-    margin-top: 2.75rem;
-  }
-`;
-
-const ReadMoreLink = styled.a`
-  display: none;
-
-  margin-top: 0.5rem;
-  margin-left: 3.8rem;
-  font-size: 12px;
-
-  @media (min-width: ${(props:any) => props.theme.screens.md}) {
-    display: flex;
-    cursor: pointer;
-  }
-
-  .arrow {
-    opacity: 0;
-    transform-origin: center center;
-    transform: rotate(-20deg) translateY(-5px);
-    transition: all 0.2s cubic-bezier(0.23, 1, 0.32, 1);
-  }
-
-  &:hover {
-    .arrow {
-      opacity: 1;
-      transform: rotate(0) translateY(0);
-    }
-  }
-`;
-
 function PostsListYears({ years }:any) {
   return (
-    <StyledPostsList className="relative z-50 max-w-xl mx-auto">
+    <div className={`${styles.StyledPostsList} relative z-50 max-w-xl mx-auto`}>
       {years.map(function (year:any) {
         return (
           <div key={year.year}>
@@ -111,19 +57,19 @@ function PostsListYears({ years }:any) {
                     <Link href={`/journal/${post.slug}`}>
                       <a className="md:text-xl font-thin truncate pr-3 flex items-end">
                         {" "}
-                        <span className="text-xs md:text-sm font-mono mr-3 text-gray-500">
+                        <span className="text-xs md:text-sm font-mono mr-3 text-gray-500 dark:text-slate-50/50">
                           {post.date}
                         </span>
                         <span className="truncate block">{post.title}</span>
                       </a>
                     </Link>
                     <Link href={`/journal/${post.slug}`}>
-                      <ReadMoreLink className="font-mono flex">
+                      <a className={`${styles.readMoreLink} font-mono flex`}>
                         <span className="mr-3">read more</span>
                         <span className="arrow">
                           <Plus className="h-5 w-5" />
                         </span>
-                      </ReadMoreLink>
+                      </a>
                     </Link>
                   </li>
                 );
@@ -132,6 +78,6 @@ function PostsListYears({ years }:any) {
           </div>
         );
       })}
-    </StyledPostsList>
+    </div>
   );
 }
