4a8bdfbc8108573396acaed7a0110dc90bbaee19
dpointeck <daniel.pointecker@gmail.com>
2023-03-25 10:53:16 +0100

    Tweak TS

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -2,6 +2,7 @@ import {remark} from 'remark';
 import remarkHTML from 'remark-html';
 import remarkPrism from 'remark-prism';
 
-export default async function markdownToHtml(markdown: any): Promise<any> {
-  return (await remark().use(remarkHTML).use(remarkPrism).process(markdown)).toString();
+export default async function markdownToHtml(markdown: string) {
+  const result = await remark().use(remarkHTML).use(remarkPrism).process(markdown);
+  return String(result);
 }
