c0a5c0dce878393c3674c5ba6d19434d272b0925
dpointeck <daniel.pointecker@gmail.com>
2023-04-17 19:39:36 +0200

    Rollback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
       router.events.off('routeChangeComplete', onRouteChangeComplete);
     };
   
-  }, [router]);
+  }, []);
 
   return (
     <>
