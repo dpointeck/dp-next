c06d9b9500018ba13d8dc2e64f72ad0901b8c22b
Daniel Pointecker <daniel.pointecker@gmail.com>
2023-05-08 15:43:16 +0200

    Move post page to app dir

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,15 +1,56 @@
-import { getAllPosts } from "@lib/post";
+import Link from "next/link";
+import PageHeader from "@components/pageHeader";
+import Plus from "@svg/Plus.js";
+import styles from "./index.module.scss";
+import { getPostsByYear } from "@lib/post";
 
-async function fetchPosts() {
-    return await getAllPosts();
+export default async function Page() {
+    const posts = await getPostsByYear();
+
+
+    return (
+    <>
+        <PageHeader>
+          <span>Journal</span>
+        </PageHeader>
+        <PostsListYears posts={posts} />
+    </>
+  );
 }
 
 
-export default async function Page() {
-    const post = await fetchPosts()
-
-
-    console.log(post)
-
-    return <div>Blog</div>;
+function PostsListYears({ posts }:any) {
+  return (
+    <div className={`${styles.StyledPostsList} relative z-50 max-w-xl mx-auto`}>
+      {posts.map(function (year:any) {
+        return (
+          <div key={year.year} className="mt-16">
+            <h2 className="font-mono text-4xl">{year.year}</h2>
+            <hr />
+            <ul>
+              {year.posts.map(function (post:any) {
+                return (
+                  <li className="mt-4" key={post.slug}>
+                    <Link href={`/journal/${post.slug}`} className="md:text-xl font-thin truncate pr-3 flex items-end">
+                        {" "}
+                        <span className="text-xs md:text-sm font-mono mr-3 text-gray-500 dark:text-slate-50/50">
+                          {post.date}
+                        </span>
+                        <span className="truncate block">{post.title}</span>
+                    </Link>
+                    <Link href={`/journal/${post.slug}`} className={`${styles.readMoreLink} font-mono flex`}>
+                        <span className="mr-3">read more</span>
+                        <span className="arrow">
+                          <Plus className="h-5 w-5" />
+                        </span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/layouts/pageLayout.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function PageLayout(props:any) {
-  return (
-    <div className="layout-page container mx-auto p-4 md:px-10">
-      {props.children}
-    </div>
-  );
-}
