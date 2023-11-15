d715b9513a7896cb5377994e0ad82ab12baecd89
dpointeck <daniel.pointecker@gmail.com>
2023-11-15 20:34:17 +0100

    Add contentlayer

--- src/lib/markdown.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-import {remark} from 'remark';
-import remarkHTML from 'remark-html';
-import remarkPrism from 'remark-prism';
-
-export async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkHTML).use(remarkPrism).process(markdown);
-  return String(result);
-}
