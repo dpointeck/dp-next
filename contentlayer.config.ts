ef32394d930aac1b7affd1e0df3f73ac9f9ed97a
Daniel Pointecker <daniel.pointecker@frauscher.com>
2024-03-26 07:10:07 +0100

    Add shiki highlighter

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,5 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import rehypePrettyCode from "rehype-pretty-code";
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -11,4 +12,23 @@ export const Post = defineDocumentType(() => ({
   },
 }))
 
-export default makeSource({ contentDirPath: '_posts', documentTypes: [Post] })
+const rehypeoptions = {
+    // Use one of Shiki's packaged themes
+    theme: "github-light",
+    // Set to true to keep the background color
+    keepBackground: true ,
+    onVisitLine(node: any) {
+      if (node.children.length === 0) {
+        node.children = [{ type: "text", value: " " }];
+      }
+    },
+    onVisitHighlightedLine(node: any) {
+      node.properties.className.push("highlighted");
+    },
+    onVisitHighlightedWord(node: any, id: any) {
+      node.properties.className = ["word"];
+    },
+  };
+
+
+export default makeSource({ contentDirPath: '_posts', documentTypes: [Post], markdown: { rehypePlugins: [[rehypePrettyCode, rehypeoptions]] as any } })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,61 @@
+
+const disabledCss = {
+    'code::before': false,
+    'code::after': false,
+    'blockquote p:first-of-type::before': false,
+    'blockquote p:last-of-type::after': false,
+    pre: false,
+    code: false,
+    'pre code': false,
+    'code::before': false,
+    'code::after': false,
+}
+
 module.exports = {
-  content: [
-    "components/**/*.{js,ts,jsx,tsx}",
-    "app/**/*.{js,ts,jsx,tsx}",
-    "pages/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: [
-        "Greycliff",
-        "Inter",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica",
-        "Arial",
-        "sans-serif",
-        "Apple Color Emoji",
-        "Segoe UI Emoji",
-        "Segoe UI Symbol",
-      ],
-      mono: [
-        "Codesaver",
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        '"Liberation Mono"',
-        '"Courier New"',
-        "monospace",
-      ],
+    content: [
+        "components/**/*.{js,ts,jsx,tsx}",
+        "app/**/*.{js,ts,jsx,tsx}",
+        "pages/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            sans: [
+                "Greycliff",
+                "Inter",
+                "-apple-system",
+                "BlinkMacSystemFont",
+                "Segoe UI",
+                "Roboto",
+                "Helvetica",
+                "Arial",
+                "sans-serif",
+                "Apple Color Emoji",
+                "Segoe UI Emoji",
+                "Segoe UI Symbol",
+            ],
+            mono: [
+                "Codesaver",
+                "Menlo",
+                "Monaco",
+                "Consolas",
+                '"Liberation Mono"',
+                '"Courier New"',
+                "monospace",
+            ],
+        },
+        extend: {
+            colors: {
+                "accent-1": "#333",
+            },
+            typography: {
+                DEFAULT: { css: disabledCss },
+                sm: { css: disabledCss },
+                lg: { css: disabledCss },
+                xl: { css: disabledCss },
+                '2xl': { css: disabledCss },
+            },
+        },
     },
-    extend: {
-      colors: {
-        "accent-1": "#333",
-      },
-    },
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/typography")],
+    variants: {},
+    plugins: [require("@tailwindcss/typography")],
 };
