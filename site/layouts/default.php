16ff7e5e3d4460d43a453a7c4bf228635aec7bdd
dpointeck <daniel.pointecker@gmail.com>
2024-05-01 15:09:38 +0200

    Add basic styles

--- site/layouts/default.php
+++ site/layouts/default.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?= vite()->css('js/index.ts') ?>
-    <?= vite()->js('js/index.ts', ['defer' => true]) ?>
+    <?= vite()->css('ts/index.ts') ?>
+    <?= vite()->js('ts/index.ts', ['defer' => true]) ?>
 </head>
 
 <body>
