d0da8e6192fd71028a3a8fb4a0d14e7b98399b33
Daniel Pointecker <daniel.pointecker@gmail.com>
2025-11-30 10:11:59 +0100

    remove vite as build tool
    i made the mistake to add in vite from the start but i am sure that i
    don't need it. so i removed it to start lean and focus on adding content
    next instead of technical stuff i just want to build for no reason.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,6 +1,6 @@
 <head>
-    <?= vite()->css('css/main.css') ?>
-    <?= vite()->js('js/main.js') ?>
+  <link href="<?= $kirby->url('assets') ?>/css/main.css" rel="stylesheet" />
+  <script defer type="module" src="<?= $kirby->url('assets') ?>/js/main.js"></script>
 </head>
 
 <h1><?= $page->title() ?></h1>
